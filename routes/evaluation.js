956ecacebf32cdcaafc54d913495e68a23b299a9
parkhojun <mara8534@naver.com>
2020-01-07 10:53:00 +0900

    -

--- routes/evaluation.js
+++ routes/evaluation.js
@@ -4,8 +4,11 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    const out1 = exec('bash run_test.sh');
-    const out2 = exec('bash run_single.sh "sample_data/images/0_L_CC.png" "L-CC"');
+    // const out1 = exec('bash run_test.sh');
+    // const out2 = exec('bash run_single.sh "sample_data/images/0_L_CC.png" "L-CC"');
+
+    const out1 = exec('pwd');
+    const out2 = exec('pwd');
 
     res.json({ "msg1": out1, "msg2": out2 });
 });
