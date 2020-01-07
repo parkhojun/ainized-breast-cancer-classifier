19c1e388dbbd70f98e80cb991209090792600f45
parkhojun <mara8534@naver.com>
2020-01-07 10:47:15 +0900

    -

--- routes/upload.js
+++ routes/upload.js
@@ -4,7 +4,7 @@ let multer = require('multer');
 let router = express.Router();
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, __dirname + '/sample_data/images')
+        cb(null, __dirname + '/sample_data/images/')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
@@ -16,6 +16,7 @@ var upload = multer({ storage: storage });
 /* GET home page. */
 router.post('/', upload.single('file'), (req, res) => {
     const { file } = req;
+    console.log(file);
     if (!file) { res.json({ "status": false }); }
     else { res.json({ "status": true }); }
 
