4afdd75c710164652db5c0e262267656bfe013ef
parkhojun <mara8534@naver.com>
2020-01-07 10:41:54 +0900

    -

--- routes/evaluation.js
+++ routes/evaluation.js
@@ -4,11 +4,10 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
     const out1 = exec('bash run_test.sh');
     const out2 = exec('bash run_single.sh "sample_data/images/0_L_CC.png" "L-CC"');
-    res.json({ "eval": out1 });
-    res.json({ "eval": out2 });
+
+    res.json({ "msg1": out1, "msg2": out2 });
 });
 
 module.exports = router;
